d33b28876a9cc63c9a77525bfffd543d191e4704
shaksham08 <shakshamsinha@outlook.com>
2022-07-21 17:11:17 +0530

    COMPLETED PROJECT

---
+++ index.js
@@ -0,0 +1,34 @@
+const { parse } = require("csv-parse");
+const fs = require("fs");
+
+const habitablePlanets = [];
+
+// Function to check for habitable planets
+function isPlanetHabitable(planet) {
+  return (
+    planet["koi_disposition"] === "CONFIRMED" &&
+    planet["koi_insol"] > 0.36 &&
+    planet["koi_insol"] < 1.11 &&
+    planet["koi_prad"] < 1.6
+  );
+}
+
+fs.createReadStream("kepler_data.csv")
+  .pipe(
+    parse({
+      comment: "#",
+      columns: true,
+    })
+  )
+  .on("data", (data) => {
+    if (isPlanetHabitable(data)) habitablePlanets.push(data);
+  })
+  .on("error", (error) => console.log(error))
+  .on("end", () => {
+    console.log(
+      habitablePlanets.map((planet) => {
+        return planet["kepler_name"];
+      })
+    );
+    console.log(`${habitablePlanets.length} habitable planets found!`);
+  });
